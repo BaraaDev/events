b3d8f1559107445864eca6e826ff16d8921517bb
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-04-13 07:49:30 +0200

    city in french (fr) done!

---
+++ lang/fr/admin/city.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'city'                          => 'Ville',
+    'cities'                        => 'Villes',
+    'all_cities'                    => 'Toutes les villes',
+    'city_content'                  => 'Contenu de la ville',
+    'content'                       => 'Le contenu',
+    'create'                        => 'Créer une ville',
+    'edit'                          => 'Éditer',
+    'cityNames'                     => 'Noms de ville',
+    'addCity'                       => 'Ajouter une ville',
+    'showCities'                    => 'Afficher les villes',
+    'name'                          => 'Nom',
+    'add_new'                       => 'Ajouter nouveau',
+    'update'                        => 'Mettre à jour',
+    'cityDescription'           => 'Toutes les villes Si vous souhaitez créer et ajouter de nouvelles villes, vous devez cliquer sur le bouton (Ajouter de nouvelles sections) en haut de la page',
+];
