41e7c0a4a7be549026e922b2e9562f2ec3cd9f93
KhalifaFullStack <96081083+KhalifaFullStack@users.noreply.github.com>
2022-04-13 07:26:53 +0200

    city lang

---
+++ lang/en/admin/city.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'city'                          => 'city',
+    'cities'                        => 'cities',
+    'all_cities'                    => 'All Cities',
+    'city_content'                  => 'About City',
+    'content'                       => 'Content',
+    'create'                        => 'Create City',
+    'edit'                          => 'Edit',
+    'cityNames'                     => 'City Names',
+    'addCity'                       => 'Add City',
+    'showCities'                    => 'Show Cities',
+    'name'                          => 'Name',
+    'add_new'                       => 'Add New',
+    'update'                        => 'Update',
+    'cityDescription'           => 'جميع المدن ان كنت ترغب في إنشاء وأضافه مدن جديده ف عليك بالضغط علي زر (أصافة أقسام جديده) في أعلي الصفحة',
+];
