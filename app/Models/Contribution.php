79708ef9e6532b09b04c30505ea10b0ab9e91a14
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-05-16 19:12:21 +0200

    some updates

--- app/Models/Contribution.php
+++ app/Models/Contribution.php
@@ -12,7 +12,7 @@ class Contribution extends Model
     use HasFactory, HasTranslations, SoftDeletes;
 
     protected $table = 'contributions';
-    public $translatable = ['title', 'content', 'category_id'];
+    public $translatable = ['title', 'content'];
 
     public function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
