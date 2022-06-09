c1efe97e8e04b66d298c92b727e428a89d8025a8
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-06-09 03:16:25 +0200

    updates

--- storage/framework/views/b4de400ceea8c36165a7a78fa83bd42f5e8b2d06.php
+++ storage/framework/views/b4de400ceea8c36165a7a78fa83bd42f5e8b2d06.php
@@ -110,7 +110,20 @@
                 <ul class="primary-menu-menu" style="width: 125%; padding-left:4%;">
                     <li class="menu-item-has-children">
                         <a href="<?php echo e(route('home')); ?>"><?php echo e(__('website/home.home')); ?></a></li>
-                        
+                        <?php if(auth()->user()->user_type == 'customer'): ?>
+                        <li>
+                            <a class="menu-component-item" href="<?php echo e(route('allEvents')); ?>"><?php echo e(__('website/home.events')); ?></a>
+                            <ul class="sub-menu">
+                                <li><a href="<?php echo e(route('myEvents')); ?>">My Events</a></li>
+                                <li><a href="<?php echo e(route('allEvents')); ?>">Other Events</a></li>
+                                <li><a href="<?php echo e(route('event.create')); ?>">Create an Event</a></li>
+                            </ul>
+                        </li>
+                        <?php else: ?>
+                        <li>
+                            <a class="menu-component-item" href="<?php echo e(route('allEvents')); ?>"><?php echo e(__('website/home.events')); ?></a>
+                        </li>
+                        <?php endif; ?>
                     </li>
 
                     <li>
