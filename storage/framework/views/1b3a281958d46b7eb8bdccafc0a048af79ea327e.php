1ade6fa80d6c843cf3b64bef5c8e5e4bb145bec8
Kareem-Tarek <kareemtarekpk@gmail.com>
2022-06-09 18:57:27 +0200

    updates

--- storage/framework/views/1b3a281958d46b7eb8bdccafc0a048af79ea327e.php
+++ storage/framework/views/1b3a281958d46b7eb8bdccafc0a048af79ea327e.php
@@ -274,7 +274,7 @@ if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
-unset($__errorArgs, $__bag); ?>" type="text" name="icon"> <!-- the link of the image icon -->
+unset($__errorArgs, $__bag); ?>" type="text" name="icon" value="<?php echo e(Request::old('icon') ? Request::old('icon') : $model->icon); ?>"> <!-- the link of the image icon -->
         <?php $__errorArgs = ['icon'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
